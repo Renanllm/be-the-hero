33a93179cf2c1126016b7ea1c0014fd171d51aaa
Renan Luiz <renanllm01@gmail.com>
2020-03-25 00:25:19 -0300

    First Commit - Backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config({
+    path: 'config/.env'
+});
+
+const express = require("express");
+const cors = require("cors");
+const routes = require('./routes/routes');
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use('/api', routes);
+
+app.listen(port, () => console.log(`Listen server in port ${port}`));
